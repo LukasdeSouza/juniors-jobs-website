d9fe988f00bff758019a663d94936346c21d5248
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2022-12-15 22:41:42 -0300

    Criação da Store para utilizar com Mobx

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Stack } from '@mui/material'
+import { Button, Stack, Typography } from '@mui/material'
 
 import JobsStack from '../stack'
 import '../../styles/global.css'
 
-
 const JobsBox = () => {
+
+  const [jobs, setJobs] = useState('')
+
+  useEffect(() => {
+    fetch('https://seek-jobs-website-api.onrender.com/jobs')
+      .then(response => response.json())
+      .then(data => setJobs(data))
+      .catch(error => console.group(error))
+  }, [])
+
   return (
-    <Stack alignItems={'center'}>
-      <JobsStack title={'Title'}
-        description={"Description to this component to describe the job"}
+    <Stack alignItems={'center'} mt={6}>
+      {jobs?.map((job) => (
+        <JobsStack title={job.title}
+          description={job.description}
+          salary={job.salary}
+          local={job.local}
+        />
+      ))}
+
+      {/* <JobsStack title={'Title'}
+        description={jobs.description}
         salary={'R$100'}
         local={'Remote'}
       />
@@ -28,7 +45,7 @@ const JobsBox = () => {
         description={"Description to this component to describe the job"}
         salary={'R$100'}
         local={'Remote'}
-      />
+      /> */}
     </Stack >
   )
 }
---
+++ src/store/jobsStore.js
@@ -0,0 +1,29 @@
+import { action, makeAutoObservable, observable } from "mobx";
+
+class JobsStore {
+
+  state = {
+    jobsList: [],
+    page: 0
+  }
+  loading = false
+
+  constructor() {
+    makeAutoObservable(this, {
+      state: observable,
+
+      setState: action,
+      setLoading: action,
+    })
+  }
+
+  setState(key, value) {
+    this.state[key] = value
+  }
+
+  setLoading(value) {
+    this.loading = value
+  }
+}
+
+export default JobsStore
