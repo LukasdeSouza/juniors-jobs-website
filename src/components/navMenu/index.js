7be69ecd76665404b255f708a31bd6ac2978d644
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2022-12-16 13:17:52 -0300

    Alterações na interface, adição de novos campos, remoção do repository, e alteração do controller

--- src/components/navMenu/index.js
+++ src/components/navMenu/index.js
@@ -2,25 +2,42 @@ import React from 'react'
 
 import { Stack, Typography } from '@mui/material'
 
+import FlightIcon from '@mui/icons-material/Flight';
+import PlaceIcon from '@mui/icons-material/Place';
+import PublicIcon from '@mui/icons-material/Public';
+
 import './style.css'
 
 const NavMenu = () => {
   return (
-    <Stack flexDirection={'row'} justifyContent={'space-evenly'}
+    <Stack
+      flexDirection={'row'}
+      justifyContent={'space-evenly'}
       mt={2}
+      mb={8}
     >
-      <Typography
-        className='default-font'
-        sx={{ fontFamily: 'Segoe UI', fontSize: 14, fontWeight: 700 }}>
-        Vagas Presenciais</Typography>
-      <Typography
-        className='default-font'
-        sx={{ fontFamily: 'Segoe UI', fontSize: 14, fontWeight: 700 }}>
-        Vagas Remotas</Typography>
-      <Typography
-        className='default-font'
-        sx={{ fontFamily: 'Segoe UI', fontSize: 14, fontWeight: 700 }}>
-        Vagas Exterior</Typography>
+      <Stack flexDirection={'row'} alignItems={'center'}>
+        <PlaceIcon sx={{ mr: 1 }} color='success' />
+        <Typography
+          className='default-font'
+          sx={{ fontFamily: 'Segoe UI', fontSize: 14 }}>
+          Vagas Presenciais</Typography>
+      </Stack>
+      <Stack flexDirection={'row'} alignItems={'center'}>
+        <PublicIcon sx={{ mr: 1 }} color='success' />
+        <Typography
+          className='default-font'
+          sx={{ fontFamily: 'Segoe UI', fontSize: 14 }}>
+          Vagas Remotas</Typography>
+      </Stack>
+      <Stack flexDirection={'row'} alignItems={'center'}>
+        <FlightIcon sx={{ mr: 1 }} color='success' />
+        <Typography
+          className='default-font'
+          sx={{ fontFamily: 'Segoe UI', fontSize: 14 }}>
+          Vagas Exterior</Typography>
+      </Stack>
+
     </Stack>
   )
 }
