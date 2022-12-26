325a67b8b2c72f6fe2a7dd83339eec193be62941
Victorprog4 <victroeeee4862@gmail.com>
2022-12-26 20:20:45 -0300

    corrigindo regra de espaçamento da página de contribuições

--- src/pages/contribute/index.js
+++ src/pages/contribute/index.js
@@ -1,66 +1,52 @@
-import { Container, Box, Typography, Button, Link } from '@mui/material';
+import { Container, Box, Typography, Button, Link } from "@mui/material";
 
-import AppBarNavigation from '../../components/appbar'
+import AppBarNavigation from "../../components/appbar";
 
-import contribuir_img from '../../assets/img-sec-contribuicao.png';
+import contribuir_img from "../../assets/img-sec-contribuicao.png";
 
-import './index.css';
+import "./index.css";
+
+import "../../styles/utility-classes.css";
 
 //página de como contribuir
 export default function ContributesPage() {
   return (
     <>
       <AppBarNavigation />
-      <Container
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          alignItems: "center",
-          paddingTop: "6em",
-        }}
-      >
+      <section className="contain-contributions py-8">
+        <div>
+          <img src={contribuir_img} />
+        </div>
 
-        <Box sx={{ maxWidth: "450px" }}>
-          <img src={contribuir_img} width={'100%'} />
-        </Box>
+        <div className="area-text-contributions">
+          <div className="separador-blue">
+            <h2 className="title-h3 ml-1">Curtiu o Site ?</h2>
+            <span className="subtitle-02 mt-1 ml-1">Ajude nos a mantê-lo!</span>
+          </div>
+          <p className="title-h5 mt-6">
+            <span className="subtitle-01">Seek Jobs</span> é um Projeto
+            Open-Source
+          </p>
+          <p className="pt-3 subtitle-02">
+            O seek jobs é um site gratuito de código aberto construído pela
+            comunidade, nosso objetivo é ajudar a comunidade de desenvolvedores
+            a encontrar emprego na área de forma simples e sem burocracia. Nosso
+            site é mantido com a ajuda de pessoas como você, então considere nos
+            ajudar.
+          </p>
 
-        <Box sx={{ maxWidth: "430px", marginLeft: { sm: "0", md: "50px" }, marginTop: { xs: "4em", md: "0" } }}>
-          <Box sx={{
-            borderLeft: "4px solid var(--blue-scale-300)",
-            paddingLeft: "1em",
-            marginBottom: "3em"
-          }}
-          >
-            <Typography variant='h4' fontSize={"2.6em"} fontFamily={'Montserrat'} fontWeight={600}>
-              Curtiu o site ?
-            </Typography>
-            <Typography variant='subtitle1' style={{ color: "var(--blue-scale-400)" }} fontFamily={'Montserrat'} fontWeight={500}>
-              Ajude-nos a mantê-lo
-            </Typography>
-          </Box>
+          <button className="btn mt-4">
+            <a href="#">Quero contribuir!</a>
+          </button>
 
-          <Typography variant='h6' fontFamily={'Montserrat'}>
-            <b style={{ color: "var(--blue-scale-400)" }}>Seek Jobs</b> é um projeto Open-Source
-          </Typography>
-          <br />
-          <Typography variant='body1' fontFamily={'Montserrat'} fontWeight={500}>
-            O seek jobs é um site gratuito de código aberto construído pela comunidade, nosso objetivo é ajudar a comunidade de desenvolvedores a encontrar emprego na área de forma simples e sem burocracia. Nosso site é mantido com a ajuda de pessoas como você, então considere nos ajudar.
-          </Typography>
-
-          <Box sx={{ textAlign: "center", marginTop: "1em" }}>
-            <Button variant="contained" sx={{ bgcolor: "var(--blue-scale-400)", height: 52, width: 420 }}>
-              <Typography fontFamily={'Montserrat'} fontWeight={600} color={'#fff'}>
-                QUERO CONTRIBUIR
-              </Typography>
-            </Button>
-            <Typography fontFamily={'Montserrat'}
-              sx={{ color: "var(--black-scale-200)", mt: 4 }}>ou pelo pix da nossa Vakinha online </Typography>
-            <Link underline='hover' fontSize={14}>3344996@vakinha.com.br</Link>
-          </Box>
-        </Box>
-
-      </Container >
+          <p className="subtitle-02 link-da-vakinha mt-4">
+            ou pelo pix da nossa Vakinha online <br></br>
+            <a href="https://www.vakinha.com.br/3344996" target="_blank">
+              3344996@vakinha.com.br
+            </a>
+          </p>
+        </div>
+      </section>
     </>
-  )
+  );
 }
