b95098de5a1519e30d6a8e514b02f21ae7b49ec0
Andersson S. Gonçalves <anderssonsilva793@gmail.com>
2022-12-26 14:33:06 -0300

    correção de código

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { AppBar, Avatar, Link, Stack } from '@mui/material';
 import './App.css';
 import Logo from './assets/logo_size_invert.jpg'
 import AppBarNavigation from './components/appbar';
-import Contributes from './pages/contributes';
 
 
 function App() {
   return (
     <>
       <AppBarNavigation />
-      <Contributes/>
     </>
   );
 }
