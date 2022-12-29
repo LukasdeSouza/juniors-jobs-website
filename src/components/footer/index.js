4dd995d1c10592f3a46c852f080815c0c17a1267
Arthur Fabris <afpeluque@gmail.com>
2022-12-29 14:58:51 -0300

    adiciona estrutura do componente

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,2 +1,41 @@
 import React from "react"
 
+import './style.css'
+
+const TempArrayApiSimulation = [
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'},
+ {gitHubLink: './', img:'', office: 'Product Maneger'}
+]
+
+const TeamMember = ({menberOffice, memberGitHubLink='', img=''}) => {
+    return (
+        /* href = memberGitHubLink */
+        <a href='./' rel='external' /* target='_blank' */ className="team-member">{menberOffice}</a>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <a href="./" rel='external' /* target='_black' */>Ajude nosso projeto a evoluir</a>
+                <a href="./" rel='external' /* target='_black' */>Contribuições e Parceiras</a>
+            </div>
+            <div>
+                <h1>Nossa equipe</h1>
+                <ul>
+                {TempArrayApiSimulation.map((e) => <TeamMember menberOffice={e.office}/>)}
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export { Footer }
