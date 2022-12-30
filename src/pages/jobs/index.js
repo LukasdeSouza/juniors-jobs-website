2630906515050d8dea02f1d73e754bcad7fb586c
richaardev <me@richaar.dev>
2022-12-30 08:06:47 -0300

    feat: new add job component and style

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -18,12 +18,15 @@ import Logo from '../../assets/logo_size-removebg.png'
 import '../../styles/global.css'
 
 import { motion } from "framer-motion"
+import NewJobModal from '../../components/newjob';
 
 
 const JobsPage = observer(() => {
 
   const { jobsStore } = useContext(RootStoreContext)
   const controller = new JobsController(jobsStore)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const [itemsPerPage, setItemsPerPage] = useState(10)
   const [currentPage, setCurrentPage] = useState(0)
@@ -49,6 +52,8 @@ const JobsPage = observer(() => {
     pagination: jobsStore.state.pagination
   }
 
+
+
   const clearFields = () => {
     jobsStore.setState('urlImage', '')
     jobsStore.setState('title', '')
@@ -58,7 +63,8 @@ const JobsPage = observer(() => {
     jobsStore.setState('local', '')
     jobsStore.setState('link', '')
     jobsStore.setState('pagination', '')
-    jobsStore.setState('openDrawer', true)
+
+    setIsModalOpen(true)
   }
 
   const onSave = () => {
@@ -106,73 +112,7 @@ const JobsPage = observer(() => {
         //   controller.getAllJobs()
         // }}
         /> */}
-      {
-        jobsStore.state.openDrawer &&
-        <motion.div
-          initial={{ x: '-100vw' }}
-          transition={{
-            duration: 1,
-            ease: "easeInOut",
-          }}
-          animate={{ x: 0 }}
-        >
-          <Drawer
-            anchor={'right'}
-            open={jobsStore.state.openDrawer}
-            onClose={() => jobsStore.setState('openDrawer', false)}
-            PaperProps={{
-              sx: {
-                background: 'white',
-                width: '350px'
-              }
-            }}
-          >
-            <Box p={2} width={305}>
-              <Stack flexDirection={'row'} alignItems={'center'} p={1}>
-                {/* <AddToQueueIcon color='warning' /> */}
-                <Typography
-                  sx={{ fontSize: 16, fontFamily: 'Poppins' }}>
-                  Cadastro de Nova Vaga
-                </Typography>
-              </Stack>
-              <Divider />
-              <Stack flexDirection={'column'} mt={2}>
-                <Typography variant='caption' sx={{ fontFamily: 'Poppins' }}>Logo da Empresa (URL)</Typography>
-                <TextField size='small'
-                  // placeholder='www.urlogodampresa.com/logo.png'
-                  onChange={(e) => jobsStore.setState('urlImage', e.target.value)} />
-                <Typography variant='caption' mt={1} sx={{ fontFamily: 'Poppins' }}>Título da Vaga</Typography>
-                <TextField size='small'
-                  // placeholder='Desenvolvedor FullStack'
-                  onChange={(e) => jobsStore.setState('title', e.target.value)} />
-                <Typography variant='caption' mt={1} sx={{ fontFamily: 'Poppins' }}>Mini Descrição</Typography>
-                <TextField size='small'
-                  // placeholder='Jr /Pleno/ CLT/PJ'
-                  onChange={(e) => jobsStore.setState('description', e.target.value)} />
-                <Typography variant='caption' mt={1} sx={{ fontFamily: 'Poppins' }}>Stacks/ Tecnologias</Typography>
-                <TextField size='small'
-                  // placeholder='JavaScript / Java'
-                  onChange={(e) => jobsStore.setState('tecnologies', e.target.value)} />
-                <Typography variant='caption' mt={1} sx={{ fontFamily: 'Poppins' }}>Salário</Typography>
-                <TextField size='small'
-                  // placeholder='R$ 2000'
-                  onChange={(e) => jobsStore.setState('salary', e.target.value)} />
-                <Typography variant='caption' mt={1} sx={{ fontFamily: 'Poppins' }}>Local</Typography>
-                <TextField size='small'
-                  // placeholder='Remote/Híbrido ou Local Presencial'
-                  onChange={(e) => jobsStore.setState('local', e.target.value)} />
-                <Typography variant='caption' mt={1} sx={{ fontFamily: 'Poppins' }}>Link da Vaga</Typography>
-                <TextField size='small'
-                  // placeholder='www.vagaparainscricao.com'
-                  onChange={(e) => jobsStore.setState('link', e.target.value)} />
-                <Button variant='contained'
-                  onClick={onSave}
-                  sx={{ backgroundColor: '#363535', mt: 2, fontFamily: 'Poppins' }}>Salvar</Button>
-              </Stack>
-            </Box>
-          </Drawer>
-        </motion.div>
-      }
+      <NewJobModal setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} controller={controller} />
     </>
   )
 }
