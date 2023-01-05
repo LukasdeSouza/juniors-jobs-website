955c1eaba03fb45741a61431d3b3aca477f67316
Afra Taiza <afrataiza@gmail.com>
2023-01-05 17:57:53 -0300

    feat: adiciona novas props Adiciona as props handleChange, função setada no evento de onChange e name, setada como name do input. Adiciona classe e atualiza estilização.

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './style.css';
 
-const Input = ({ type, placeHolder }) => {
+const Input = ({ type, placeHolder, name, handleChange }) => {
     return (
         <input
+        className="pl-2 input-component"
         type={type}
         placeholder={placeHolder}
+        name={name}
+        onChange={handleChange}
         />
     );
 }
