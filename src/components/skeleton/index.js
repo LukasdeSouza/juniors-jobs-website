aa3c31afa1c1b862de969b5949822bfa08348a7c
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-01-11 15:22:45 -0300

    removing some skeletons for a moment

--- src/components/skeleton/index.js
+++ src/components/skeleton/index.js
@@ -27,30 +27,6 @@ const JobsSkeleton = () => {
           <Skeleton variant='rectangular' width={'424px'} height={"40px"} sx={{ mt: 2, borderRadius: 2 }} />
         </Stack>
       </Stack>
-      <Stack
-        mt={6}
-        flexDirection={'row'}
-        justifyContent={'center'}>
-        <Skeleton variant='circular' width={69} height={69} sx={{ mr: 2 }} />
-        <Stack flexDirection={'column'}>
-          <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={'524px'} height={"30px"} />
-          <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={'124px'} height={"30px"} />
-          <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={'224px'} height={"30px"} />
-          <Skeleton variant='rectangular' width={'424px'} height={"40px"} sx={{ mt: 2, borderRadius: 2 }} />
-        </Stack>
-      </Stack>
-      <Stack
-        mt={6}
-        flexDirection={'row'}
-        justifyContent={'center'}>
-        <Skeleton variant='circular' width={69} height={69} sx={{ mr: 2 }} />
-        <Stack flexDirection={'column'}>
-          <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={'424px'} height={"30px"} />
-          <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={'124px'} height={"30px"} />
-          <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={'224px'} height={"30px"} />
-          <Skeleton variant='rectangular' width={'424px'} height={"40px"} sx={{ mt: 2, borderRadius: 2 }} />
-        </Stack>
-      </Stack>
     </>
   )
 }
