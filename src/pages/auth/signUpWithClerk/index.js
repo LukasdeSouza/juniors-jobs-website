daa54f630d2171f9b4a6742f0d89491164dd2753
LukasdeSouza <lucas@bestdeal.com.br>
2023-07-11 19:49:46 -0300

    fix: tentativa de deploy novamente com chave pública no netlify

--- src/pages/auth/signUpWithClerk/index.js
+++ src/pages/auth/signUpWithClerk/index.js
@@ -2,6 +2,7 @@ import { SignUp } from '@clerk/clerk-react'
 import './style.css'
 
 const SignUpWithClerk = () => {
+  console.log('signup')
   return (
     <div className="signInWithClerk-container">
       <SignUp />
