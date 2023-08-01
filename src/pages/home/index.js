3090447f2c771546e003544652e05f931019481d
LukasdeSouza <lucas@bestdeal.com.br>
2023-08-01 14:29:44 -0300

    fix: já tava corrigido kk

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -23,6 +23,7 @@ const HomePage = () => {
           </h2>
           <span className="left-container-span-text">
             Reunimos diversas vagas de diferentes plataformas em um só lugar. <br />
+            Chega de vasculhar site por site, ou ter que filtrar vagas que encaixem em um perfil iniciante
           </span>
           <br />
           <Button
